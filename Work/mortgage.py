8221f8d81264520a86cb2935f49cd6ae9dec0e09
pilaster777 <felix.meessen@gmx.de>
2025-02-09 14:34:31 +0100

    Exercise 1.17: f-strings

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,15 +1,15 @@
 # mortgage.py
 #
-# Exercise 1.11: Bonus
+# Exercise 1.17: f-strings
 principal = 500000
 rate = 0.05
 
 total_paid = 0.0
 
-if input("Extra payment? Y/n ") == "Y":
-    extra_payment_start_month = int(input("extra_payment_start_month "))
-    extra_payment_end_month = int(input("extra_payment_end_month "))
-    extra_payment = int(input("extra_payment "))
+if input('Extra payment? Y/n ') == 'Y':
+    extra_payment_start_month = int(input('extra_payment_start_month '))
+    extra_payment_end_month = int(input('extra_payment_end_month '))
+    extra_payment = int(input('extra_payment '))
 else:
     extra_payment_start_month = 0
     extra_payment_end_month = 0
@@ -28,7 +28,9 @@ while principal > 0:
     principal = principal * ( 1 + rate / 12 ) - payment
     total_paid = total_paid + payment
     month += 1
-    print(month, "   ",round(total_paid,2), "   ", round(principal,2))
-print("Total paid ",round(total_paid,2))
-print("Months ",month)
+    
+    print(f'Month \t{month} \tTotal payment: \t{total_paid:0.2f} \tprincipal: \t{principal:0.2f}')
+    
+print(f'Total paid: \t{total_paid:0.2f}')
+print(f'Months \t\t{month}')
     
