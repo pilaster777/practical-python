d9d998389a2c18df22f900323efb1eeedf856d6a
pilaster777 <felix.meessen@gmx.de>
2025-02-09 13:23:10 +0100

    Exercise 1.11: Bonus

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,24 +1,34 @@
 # mortgage.py
 #
-# Exercise 1.7
+# Exercise 1.11: Bonus
 principal = 500000
 rate = 0.05
 
 total_paid = 0.0
-extra_payment_start_month = input("extra_payment_start_month")
-extra_payment_end_month = input("extra_payment_end_month")
-extra_payment = input("extra_payment")
+
+if input("Extra payment? Y/n ") == "Y":
+    extra_payment_start_month = int(input("extra_payment_start_month "))
+    extra_payment_end_month = int(input("extra_payment_end_month "))
+    extra_payment = int(input("extra_payment "))
+else:
+    extra_payment_start_month = 0
+    extra_payment_end_month = 0
+    extra_payment = 0
 
 month = 0
 
 while principal > 0:
     payment = 2684.11
-    
-    if int(extra_payment) > 0 and (int(extra_payment_start_month) <= int(month) <= int(extra_payment_end_month)):
-        payment = payment + int(extra_payment)
+    if extra_payment_start_month != [None,'',0] or extra_payment_end_month != [None,'',0]:
+        if extra_payment > 0: 
+            if extra_payment_start_month <= month <= extra_payment_end_month:
+                payment = payment + extra_payment
+    if principal < payment:
+        payment = principal * ( 1 + rate / 12 )
     principal = principal * ( 1 + rate / 12 ) - payment
     total_paid = total_paid + payment
     month += 1
+    print(month, "   ",round(total_paid,2), "   ", round(principal,2))
+print("Total paid ",round(total_paid,2))
+print("Months ",month)
     
-print('Total paid', round(total_paid,1))
-print('Total time', month // 12,  " Year(s), ", month % 12, " Month(s) ..... or ",month," month(s)")
