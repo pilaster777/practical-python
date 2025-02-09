12bd1e89e5275f4c73ca9d0beecd045c48a0c04c
pilaster777 <felix.meessen@gmx.de>
2025-02-09 01:03:37 +0100

    first exercises until Exercise 1.9

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,8 @@
 # bounce.py
 #
 # Exercise 1.5
+height = 100 #unit is meter
+b_rate = 3/5 # bouncing rate
+for i in range(10):
+    height = height * b_rate
+    print(round(height,4))
--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,24 @@
 # mortgage.py
 #
 # Exercise 1.7
+principal = 500000
+rate = 0.05
+
+total_paid = 0.0
+extra_payment_start_month = input("extra_payment_start_month")
+extra_payment_end_month = input("extra_payment_end_month")
+extra_payment = input("extra_payment")
+
+month = 0
+
+while principal > 0:
+    payment = 2684.11
+    
+    if int(extra_payment) > 0 and (int(extra_payment_start_month) <= int(month) <= int(extra_payment_end_month)):
+        payment = payment + int(extra_payment)
+    principal = principal * ( 1 + rate / 12 ) - payment
+    total_paid = total_paid + payment
+    month += 1
+    
+print('Total paid', round(total_paid,1))
+print('Total time', month // 12,  " Year(s), ", month % 12, " Month(s) ..... or ",month," month(s)")
