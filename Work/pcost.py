f3f9fa727e3064e0bb2a599776b9992995b45a37
pilaster777 <felix.meessen@gmx.de>
2025-02-09 21:27:06 +0100

    # Exercise 1.32

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,15 +1,16 @@
 # pcost.py
 #
-# Exercise 1.30
-
+# Exercise 1.32
+import csv
 def portfolio_cost(filename):
     f = open(filename, 'rt')
-    headers = next(f).split(',')
+    rows = csv.reader(f)
+    headers = next(rows)
     cost = 0
-    for line in f:
-        row = line.split(',')
+    for row in rows:
         cost = cost + int(row[1]) * float(row[2])
     f.close()
     return cost
+
 cost = portfolio_cost('Data/portfolio.csv')
 print(f"Total cost: {cost}")
