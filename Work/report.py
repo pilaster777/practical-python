58aa4c8c70ed92539f6f070723044db35f593ffc
Pilaster777 <felix.meessen@gmx.de>
2025-02-10 15:01:14 +0100

    Exercise 2.5: List of Dictionaries

--- Work/report.py
+++ Work/report.py
@@ -1,20 +1,27 @@
 # report.py
 #
-# Exercise 2.4
+# Exercise 2.5
 import csv
+from pprint import pprint
 
 def portfolio_cost(filename):
     '''Computes the total cost (shares*price) of a portfolio file'''
+
     portfolio = []
     total = 0.0
+
     with open(filename, 'rt') as f:
         rows = csv.reader(f)
         headers = next(rows)
+
         for row in rows:
-            holding = (row[0], int(row[1]), float(row[2]))
+            holding = {}
+            holding['name'], holding['shares'], holding['price'] = row[0], int(row[1]), float(row[2])
             portfolio.append(holding)
-        for name, shares, price in portfolio:
-            total += shares*price
+
+        for i in range(len(portfolio)):
+            total += int(portfolio[i]['shares'])*float(portfolio[i]['price'])
+
     print(total)
 
 portfolio_cost('Data/portfolio.csv')
