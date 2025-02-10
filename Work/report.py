679c21ecdec498758401e11ea115fdff2c8d8853
Pilaster777 <felix.meessen@gmx.de>
2025-02-10 13:08:19 +0100

    Exercise 2.4

--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,20 @@
 # report.py
 #
 # Exercise 2.4
+import csv
+
+def portfolio_cost(filename):
+    '''Computes the total cost (shares*price) of a portfolio file'''
+    portfolio = []
+    total = 0.0
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
+            holding = (row[0], int(row[1]), float(row[2]))
+            portfolio.append(holding)
+        for name, shares, price in portfolio:
+            total += shares*price
+    print(total)
+
+portfolio_cost('Data/portfolio.csv')
