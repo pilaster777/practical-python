8130ee456b5f0a5d4b4fe4eb2e3a1d5871eeba59
Pilaster777 <felix.meessen@gmx.de>
2025-02-10 16:15:11 +0100

    Exercise 2.6 and 2.7

--- Work/report.py
+++ Work/report.py
@@ -1,13 +1,30 @@
 # report.py
 #
-# Exercise 2.5
+# Exercise 2.6 and 2.7
 import csv
 from pprint import pprint
+
+
+def read_prices(filename):
+
+    prices = {} # Initial empty dict
+
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        for row in rows:
+            #print(row)
+            if row != []:
+                prices[row[0]]= float(row[1])
+            else:continue
+            
+    return(prices)
+
 
 def portfolio_cost(filename):
     '''Computes the total cost (shares*price) of a portfolio file'''
 
     portfolio = []
+    t_gain_loss = 0.0
     total = 0.0
 
     with open(filename, 'rt') as f:
@@ -15,13 +32,19 @@ def portfolio_cost(filename):
         headers = next(rows)
 
         for row in rows:
-            holding = {}
-            holding['name'], holding['shares'], holding['price'] = row[0], int(row[1]), float(row[2])
-            portfolio.append(holding)
-
+            hldg = {}
+            hldg['name'], hldg['shares'], hldg['price'] = row[0], int(row[1]), float(row[2])
+            portfolio.append(hldg)
+        
+        prices = read_prices('Data/prices.csv')
+        
         for i in range(len(portfolio)):
+            t_gain_loss += int(portfolio[i]['shares'])*(float(portfolio[i]['price']) - float(prices[portfolio[i]['name']]))
             total += int(portfolio[i]['shares'])*float(portfolio[i]['price'])
 
-    print(total)
+    if t_gain_loss < 0:
+        print(f'total: {total} \tIt\'s a loss of {t_gain_loss}')
+    else:
+        print(f'Current value of my stock: \t{total} \nIt\'s a win of \t\t\t{t_gain_loss}')
 
 portfolio_cost('Data/portfolio.csv')
