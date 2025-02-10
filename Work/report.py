490ecb8051b3107d5e31578f42f17b6507a95bbc
Pilaster777 <felix.meessen@gmx.de>
2025-02-10 17:32:54 +0100

    Exercise 2.10 and 2.11, added headers

--- Work/report.py
+++ Work/report.py
@@ -1,15 +1,15 @@
 # report.py
 #
-# Exercise 2.6 and 2.7
+# Exercise 2.10 and 2.11
 import csv
 from pprint import pprint
 
 
-def read_prices(filename):
+def read_prices(pricelist_filename):
 
     prices = {} # Initial empty dict
 
-    with open(filename, 'rt') as f:
+    with open(pricelist_filename, 'rt') as f:
         rows = csv.reader(f)
         for row in rows:
             #print(row)
@@ -20,14 +20,14 @@ def read_prices(filename):
     return(prices)
 
 
-def portfolio_cost(filename):
+def portfolio_cost(stock_filename,pricelist_filename):
     '''Computes the total cost (shares*price) of a portfolio file'''
-
+    res = []
     portfolio = []
     t_gain_loss = 0.0
     total = 0.0
 
-    with open(filename, 'rt') as f:
+    with open(stock_filename, 'rt') as f:
         rows = csv.reader(f)
         headers = next(rows)
 
@@ -36,15 +36,44 @@ def portfolio_cost(filename):
             hldg['name'], hldg['shares'], hldg['price'] = row[0], int(row[1]), float(row[2])
             portfolio.append(hldg)
         
-        prices = read_prices('Data/prices.csv')
+        prices = read_prices(pricelist_filename)
         
         for i in range(len(portfolio)):
-            t_gain_loss += int(portfolio[i]['shares'])*(float(portfolio[i]['price']) - float(prices[portfolio[i]['name']]))
-            total += int(portfolio[i]['shares'])*float(portfolio[i]['price'])
 
+            shares = int(portfolio[i]['shares'])
+            old_price = float(portfolio[i]['price'])
+            new_price = float(prices[portfolio[i]['name']])
+            name = portfolio[i]['name']
+
+            change = new_price - old_price
+            gain_loss = shares * change
+            t_gain_loss += gain_loss
+            total += shares * new_price
+            
+            res.append((name,shares,new_price,change))
+    
     if t_gain_loss < 0:
-        print(f'total: {total} \tIt\'s a loss of {t_gain_loss}')
+        print(f'\nTotal worth of stock: {total:10.2f} \tIt\'s a loss of {t_gain_loss:10.2f}\n\n')
     else:
-        print(f'Current value of my stock: \t{total} \nIt\'s a win of \t\t\t{t_gain_loss}')
+        print(f'Current value of my stock: \t{total:10.2f} \nIt\'s a win of \t\t\t{t_gain_loss:10.2f}\n\n')
+    
+    return(res)
 
-portfolio_cost('Data/portfolio.csv')
+
+def make_report(stock_filename, pricelist_filename):
+    listi = portfolio_cost('Data/portfolio.csv','Data/prices.csv')
+    return listi
+
+headers = ('Name', 'Shares', 'Price', 'Change')
+ct = 0 
+sep = '----------'
+for name, shares, price, change in make_report('Data/portfolio.csv','Data/prices.csv'):
+        if ct % 10 == 0:
+            print(f'{headers[0]:>10s} {headers[1]:>10s} {headers[2]:>10s} {headers[3]:>10s}')
+            print(f'{sep:>10s} {sep:>10s} {sep:>10s} {sep:>10s}')
+        print(f'{name:>10s} {shares:>10d} {price:>10.2f} {change:>10.2f}')
+        ct +=1
+       
+
+
+    
