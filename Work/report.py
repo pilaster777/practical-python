0ca42970727292cd564fe8e9c935c578dc6591f8
Pilaster777 <felix.meessen@gmx.de>
2025-02-12 13:54:30 +0100

    Exercise 2.16: implementing zip() into portfolio_cost()

--- Work/report.py
+++ Work/report.py
@@ -22,9 +22,8 @@ def read_prices(pricelist_filename):
 
 def portfolio_cost(stock_filename,pricelist_filename):
     '''Computes the total worth (shares*price) of a portfolio file and the actual gain/loss'''
-    res = []            # result to return: (name,shares,new_price,change)
+    
     portfolio = []
-    t_gain_loss = 0.0
     total = 0.0
 
     with open(stock_filename, 'rt') as f:
@@ -32,51 +31,55 @@ def portfolio_cost(stock_filename,pricelist_filename):
         headers = next(rows)    # skip header row and save headers
 
         for row_c, row in enumerate(rows, start=1):
-            hldg = {}  
-            try:            # in case some values are missing
-                hldg['name'], hldg['shares'], hldg['price'] = row[0], int(row[1]), float(row[2])
-            except ValueError:
-                print(f'Row {row_c}: Couldn\'t convert (missing values): {row}')
-            if hldg != {}:  #exclude empty dicts
-                portfolio.append(hldg)
-
-        prices = read_prices(pricelist_filename)
-        
-        for i in range(len(portfolio)):     # calculate total worth and gain/loss
-            shares = int(portfolio[i]['shares'])
-            old_price = float(portfolio[i]['price'])
-            new_price = float(prices[portfolio[i]['name']])
-            name = portfolio[i]['name']
-
-            change = new_price - old_price
-            gain_loss = shares * change
-            t_gain_loss += gain_loss
-            total += shares * new_price
+            record = dict(zip(headers, row))
             
-            res.append((name,shares,new_price,change))
+            try:
+                nshares = int(record['shares'])
+                price = float(record['price'])
+                total += nshares * price
+            except ValueError:pass
+                #print(f'Row {row_c}: Couldn\'t convert: {row}')
+            if record != {}:  #exclude empty dicts
+                portfolio.append(record)
+        print("Total worth of portfolio:",total)
+        
     
     
     
-    return(res,t_gain_loss, total)
+    return(portfolio)
 
 
-def make_report(stock_filename, pricelist_filename):            # just simple piping of (name,shares,new_price,change)
-                                                                # and printing a report
-    po_data = portfolio_cost(stock_filename,pricelist_filename)
-    res = po_data[0]
-    total = po_data[2]
-    t_gain_loss = po_data[1]
+def make_report(stock_filename, pricelist_filename):            #
+    res = []
+    t_gain_loss = 0                                                           
+    record = portfolio_cost(stock_filename,pricelist_filename)
     
+    prices = read_prices(pricelist_filename)
+    for rowno,row in enumerate(record):     
+        try:
+            shares = int(row['shares'])
+            old_price = float(row['price'])
+            new_price = float(prices[row['name']])
+        except ValueError:
+            print(f'Row {rowno}: Bad row: {row}')
+        
+        change = new_price - old_price
+        gain_loss = shares * change
+        t_gain_loss += gain_loss
+        
+        res.append((row['name'],shares,new_price,change))
+
     if t_gain_loss < 0:
-        print(f'\nTotal worth of stock: {total:10.2f} \tIt\'s a loss of {t_gain_loss:10.2f}\n\n')
+        t_gain_loss = t_gain_loss * -1
+        print(f'\n\tIt\'s a loss of {t_gain_loss:10.2f}\n\n')
     else:
-        print(f'Current value of my stock: \t{total:10.2f} \nIt\'s a win of \t\t\t{t_gain_loss:10.2f}\n\n')
+        print(f'\n\nIt\'s a win of \t\t\t{t_gain_loss:10.2f}\n\n')
     return res
 
 headers = ('Name', 'Shares', 'Price', 'Change')
 ct = 0 
 sep = '----------'
-for name, shares, price, change in make_report('Data/missing.csv','Data/prices.csv'):
+for name, shares, price, change in make_report('Data/portfoliodate.csv','Data/prices.csv'):
         price = '$'+str(price)
         if ct % 10 == 0:                    # print out with headers every 10 rows
             print(f'{headers[0]:>10s} {headers[1]:>10s} {headers[2]:>10s} {headers[3]:>10s}')
